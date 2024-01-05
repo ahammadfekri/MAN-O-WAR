fbb2cf2f755ae4cb9e646323a739eec8cdd784a5
Mahmudur Rahman <mroyon@gmail.com>
2024-01-05 19:57:45 +0300

    test

--- Digi Com/AppForms/Cryptography.cs
+++ Digi Com/AppForms/Cryptography.cs
@@ -131,58 +131,75 @@ namespace Digi_Com.AppForms
         /// <param name="password"></param>
         public void FileDecrypt(string inputFile, string outputFile, string password)
         {
-            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
-            byte[] salt = new byte[32];
+            string fileName = string.Empty;
 
-            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
-            fsCrypt.Read(salt, 0, salt.Length);
-
-            RijndaelManaged AES = new RijndaelManaged();
-            AES.KeySize = 256;
-            AES.BlockSize = 128;
-            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
-            AES.Key = key.GetBytes(AES.KeySize / 8);
-            AES.IV = key.GetBytes(AES.BlockSize / 8);
-            AES.Padding = PaddingMode.PKCS7;
-            AES.Mode = CipherMode.CFB;
-
-            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
-
-            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
-
-            int read;
-            byte[] buffer = new byte[1048576];
-
-            try
+            clsEncLibrary objEncDec2 = new clsEncLibrary();
+            DataTable _data = _db.getScheduleListByDate(DateTime.Now.ToString("yyyy-MM-dd"));
+            foreach (DataRow row in _data.Rows)
             {
-                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    Application.DoEvents();
-                    fsOut.Write(buffer, 0, read);
-                }
-            }
-            catch (CryptographicException ex_CryptographicException)
-            {
-                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
+
+                string callerCode = Global.personel_fingre_key_no;
+                DateTime truncatedDateTime = DateTime.Now;
+                string frequency = row["SCHEDULE_FREQ"].ToString();
+                string secrateKey = row["SCHEDULE_SECRET"].ToString();
+                string filePath = inputFile;// @"C:\CF\sampleFile\Free_Test_Data_100KB_PDF.pdf";
+                fileName = objEncDec2.FileDeccrypt(filePath);
             }
 
-            try
-            {
-                cs.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
-            }
-            finally
-            {
-                fsOut.Close();
-                fsCrypt.Close();
-            }
+
+
+            //byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            //byte[] salt = new byte[32];
+
+            //FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            //fsCrypt.Read(salt, 0, salt.Length);
+
+            //RijndaelManaged AES = new RijndaelManaged();
+            //AES.KeySize = 256;
+            //AES.BlockSize = 128;
+            //var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            //AES.Key = key.GetBytes(AES.KeySize / 8);
+            //AES.IV = key.GetBytes(AES.BlockSize / 8);
+            //AES.Padding = PaddingMode.PKCS7;
+            //AES.Mode = CipherMode.CFB;
+
+            //CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            //FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            //int read;
+            //byte[] buffer = new byte[1048576];
+
+            //try
+            //{
+            //    while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+            //    {
+            //        Application.DoEvents();
+            //        fsOut.Write(buffer, 0, read);
+            //    }
+            //}
+            //catch (CryptographicException ex_CryptographicException)
+            //{
+            //    Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error: " + ex.Message);
+            //}
+
+            //try
+            //{
+            //    cs.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            //}
+            //finally
+            //{
+            //    fsOut.Close();
+            //    fsCrypt.Close();
+            //}
         }
     }
 
